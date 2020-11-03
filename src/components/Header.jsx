924e09a6d4d8b8fc03710c5f282142014300ad0b
Nirweint <nirweint_1@mail.ru>
2020-11-03 16:59:30 +0300

    more style

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://seeklogo.net/wp-content/uploads/2019/01/dji-logo.png' alt='logo'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+            <img src='https://www.ippanetwork.org/wp-content/uploads/2016/03/MC-2016-Background-e1458413633484.jpg' alt='img'/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                my posts 
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
