6d9abd887732395dff535176bd6c703352dd4a1e
Nirweint <nirweint_1@mail.ru>
2020-11-16 21:12:59 +0300

    add oop in state, like redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { addPost, updateNewPostText } from "./redux/state";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>,
         document.getElementById("root")
     );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 reportWebVitals();
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,119 +1,120 @@
-let rerenderEntireTree = () => {
-    console.log("Sataetf");
-};
-
-let state = {
-    profileState: {
-        postData: [
-            { id: "1", message: "Hi, how are you?", likeCount: "2" },
-            { id: "2", message: "It's my first post", likeCount: "7" },
-        ],
-        newPostText: "Enter message",
+let store = {
+    _state: {
+        profileState: {
+            postData: [
+                { id: "1", message: "Hi, how are you?", likeCount: "2" },
+                { id: "2", message: "It's my first post", likeCount: "7" },
+            ],
+            newPostText: "Enter message",
+        },
+        dialogsState: {
+            dialogsData: [
+                {
+                    id: 1,
+                    name: "Alex",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: 2,
+                    name: "Sofya",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: 3,
+                    name: "Pasha",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: 4,
+                    name: "Oleg",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: 5,
+                    name: "Olya",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: 6,
+                    name: "Andrey",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+            ],
+            messagesData: [
+                {
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                    id: 1,
+                    message: "Hello",
+                },
+                {
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                    id: 2,
+                    message: "hello. How are you?",
+                },
+                {
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                    id: 3,
+                    message: "too much shit happening",
+                },
+            ],
+        },
+        navFriends: {
+            friendsIcons: [
+                {
+                    id: "1",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: "1",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+                {
+                    id: "1",
+                    icon:
+                        "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
+                },
+            ],
+            friendsNames: [
+                { id: 1, name: "Alex" },
+                { id: 2, name: "Ksenya" },
+                { id: 3, name: "Sofya" },
+            ],
+        },
     },
-    dialogsState: {
-        dialogsData: [
-            {
-                id: 1,
-                name: "Alex",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: 2,
-                name: "Sofya",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: 3,
-                name: "Pasha",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: 4,
-                name: "Oleg",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: 5,
-                name: "Olya",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: 6,
-                name: "Andrey",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-        ],
-        messagesData: [
-            {
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-                id: 1,
-                message: "Hello",
-            },
-            {
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-                id: 2,
-                message: "hello. How are you?",
-            },
-            {
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-                id: 3,
-                message: "too much shit happening",
-            },
-        ],
+    getState() {
+        return this._state;
     },
-    navFriends: {
-        friendsIcons: [
-            {
-                id: "1",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: "1",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-            {
-                id: "1",
-                icon:
-                    "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png",
-            },
-        ],
-        friendsNames: [
-            { id: 1, name: "Alex" },
-            { id: 2, name: "Ksenya" },
-            { id: 3, name: "Sofya" },
-        ],
+    _callSubscriber() {
+        console.log("Sataetf");
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profileState.newPostText,
+            likeCount: 0,
+        };
+        this._state.profileState.postData.push(newPost);
+        this._state.profileState.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profileState.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 };
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profileState.newPostText,
-        likeCount: 0,
-    };
-    state.profileState.postData.push(newPost);
-    state.profileState.newPostText = "";
-    rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-    state.profileState.newPostText = newText;
-    rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
