105ff70f91e110b910c15dfa33b55ee3fd1ad821
Nirweint <nirweint_1@mail.ru>
2020-12-15 21:30:35 +0300

    users-reduser done

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,33 +1,95 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
+const SET_USERS = "SET-USERS";
 
 let initialState = {
     users: [
-        { id: 1, followed: false, fullName: "Alex", status: "I am dead", loacation: {city: "Pinsk", country: "Belarus"} },
-        { id: 2, followed: true, fullName: "Sofya", status: "I am dead too", loacation: {city: "Pinsk", country: "Belarus"} },
-        { id: 3, followed: false, fullName: "Pasha", status: "I drunk", loacation: {city: "Minsk", country: "Belarus"} },
-        { id: 4, followed: true, fullName: "Dmitry", status: "away", loacation: {city: "Moscow", country: "Russia"} },
+        {
+            id: 1,
+            followed: false,
+            fullName: "Alex",
+            status: "I am dead",
+            loacation: { city: "Pinsk", country: "Belarus" },
+        },
+        {
+            id: 2,
+            followed: true,
+            fullName: "Sofya",
+            status: "I am dead too",
+            loacation: { city: "Pinsk", country: "Belarus" },
+        },
+        {
+            id: 3,
+            followed: false,
+            fullName: "Pasha",
+            status: "I drunk",
+            loacation: { city: "Minsk", country: "Belarus" },
+        },
+        {
+            id: 4,
+            followed: true,
+            fullName: "Dmitry",
+            status: "away",
+            loacation: { city: "Moscow", country: "Russia" },
+        },
     ],
     newPostText: "",
 };
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                // users: [...state.users] same us users: [state.users.map(u => u)]
+                users: [
+                    state.users.map((u) => {
+                        if (u.id === action.userId) {
+                            return { ...u, followed: true };
+                        }
+                        return u;
+                    }),
+                ],
+            };
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: [
+                    state.users.map((u) => {
+                        if (u.id === action.userId) {
+                            return { ...u, followed: false };
+                        }
+                        return u;
+                    }),
+                ],
+            };
+        case SET_USERS: {
+            return { ...state, users: [...state.users, ...action.users] };
+        }
+
         default:
             return state;
     }
 };
 
-export const followAC = () => {
+export const followAC = (userId) => {
     return {
         type: FOLLOW,
+        userId,
     };
 };
 
-export const unfollowAC = () => {
+export const unfollowAC = (userId) => {
     return {
         type: UNFOLLOW,
+        userId,
     };
 };
 
+export const setUsersAC = (users) => {
+    return {
+        type: SET_USERS,
+        users,
+    };
+};
 export default usersReducer;
