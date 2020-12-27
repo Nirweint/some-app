1df8e47757c042060ea6ca51317ca2dcd93dc7dc
Nirweint <nirweint_1@mail.ru>
2020-12-27 17:52:24 +0300

    add withRouter in profile

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App(props) {
                     render={() => <DialogsContainer store={props.store} />}
                 />
                 <Route
-                    path="/profile"
+                    path="/profile/:userId?"
                     render={() => <ProfileContainer store={props.store} />}
                 />
                 <Route
