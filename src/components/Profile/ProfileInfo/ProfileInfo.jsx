703cebcd36a2bcc204ff871114219196f5aa1110
Nirweint <nirweint_1@mail.ru>
2020-12-27 16:31:54 +0300

    add bootstrap to progect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,16 +4,18 @@ import "./ProfileInfo.css";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
-        return <Loader/>
+        return <Loader />;
     }
- 
+
     return (
         <div className="profile-info">
-            <img className="profile-info__img"
+            <img
+                className="profile-info__img"
                 src={props.profile.photos.large}
                 alt="img"
             />
-            <div className="descriptionBlock">ava + descr</div>
+            <div className="profile-full-name">FullName: {props.profile.fullName}</div>
+            <div className="profile-about-me">Description: {props.profile.aboutMe}</div>
         </div>
     );
 };
