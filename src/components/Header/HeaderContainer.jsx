57bddc4b0b1b65055ade346c65aa29eafb3668a8
Nirweint <nirweint_1@mail.ru>
2021-01-15 23:09:13 +0300

    add some fix in headerContainer

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,15 +1,13 @@
-import Axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
+import { authAPI } from "../../api/api";
 import { setAuthUserData } from "../../redux/auth-reducer";
 import Header from "./Header";
 import "./Header.css";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        Axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-            withCredentials: true,
-        }).then((response) => {
+        authAPI.me().then((response) => {
             if (response.data.resultCode === 0) {
                 let { id, email, login } = response.data.data;
                 this.props.setAuthUserData(id, email, login);
