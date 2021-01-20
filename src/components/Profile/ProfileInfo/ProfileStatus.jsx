a7e382081635ea32e3f00df28a6d8573769d2cbf
Nirweint <nirweint_1@mail.ru>
2021-01-20 16:11:27 +0300

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -21,9 +21,18 @@ class ProfileStatus extends React.Component {
 
     onStatusChange = (e) => {
         this.setState({
-            status: e.currentTarget.value
+            status: e.currentTarget.value,
         });
     };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+        console.log("componentDidUpdate");
+    }
 
     render() {
         return (
