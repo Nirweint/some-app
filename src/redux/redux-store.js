82fc56d6ba6a3347938499c0a5c8655f1d69bb14
Nirweint <nirweint_1@mail.ru>
2021-01-21 17:58:33 +0300

    redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,6 +4,7 @@ import navFriendsReducer from "./nav-friends-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 const { createStore, combineReducers, applyMiddleware } = require("redux");
 
@@ -13,6 +14,7 @@ let reducers = combineReducers({
     navFriends: navFriendsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
